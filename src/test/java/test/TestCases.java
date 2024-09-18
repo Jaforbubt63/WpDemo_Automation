9fbe28038b957ae4146da8948a4757973cb18b13
Abu Jafor <103594222+Jaforbubt63@users.noreply.github.com>
2024-09-18 23:50:19 +0600

    change

--- src/test/java/test/TestCases.java
+++ src/test/java/test/TestCases.java
@@ -26,17 +26,19 @@ public class TestCases extends BaseDriver {
 
 
         loginPage.perform_login("Admin", "Admin123");
+        Assert.assertEquals(driver.getCurrentUrl(),"http://wpword.local/wp-admin/");
         pluginPage.click_on_plugin_button();
         wpDarkModePage.click_on_woDarkMode_in_button();
+        Assert.assertEquals(driver.getCurrentUrl(),"http://wpword.local/wp-admin/admin.php?page=wp-dark-mode#/admin");
         wpSettingsPage.click_on_wpSettings_in_button();
-        adminPanelPage.click_on_adminPanel_in_button();
-        enableAdminDashboardPage.click_on_enableAdminDashboardpage_in_button();
+        adminPanelPage.click_on_adminPanel_button();
+        enableAdminDashboardPage.click_on_enableAdminDashboard_in_button();
         customizationPage.click_on_customization_in_button();
-        customizationSwitchSettingsPage.click_on_customizationswitchSetting_in_button();
+        customizationSwitchSettingsPage.click_on_customizationSwitchSetting_in_button();
         floatingSwitchPage.click_on_floatingSwitch_in_button();
         floatingSwitchStylePage.click_on_floatingSwitchStyle_in_button();
-        floatingSwitchSizePage.click_on_floatingSwithSize_in_button();
-        floatingSwitchPositionPage.click_on_floatingSwitchSize_in_button();
+        floatingSwitchSizePage.click_on_floatingSize_in_button();
+        floatingSwitchPositionPage.click_on_floatingSwitchPosition_in_button();
         customizationSiteAnimationPage.click_on_customizationSiteAnimation_in_button();
     }
 }
